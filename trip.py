be9cc3806dccb35d20e4d870296a0c31b7e0a9c7
Maria Teresa <mariateresacappella@gmail.com>
2025-04-29 15:06:43 +0200

    Add files via upload

---
+++ trip.py
@@ -0,0 +1,74 @@
+trip = [
+    {
+        "tappa": "Arabia Saudita (jeddah)",
+        "foto": "static/jedda.jpg",
+        "giorni": [
+            {
+    "data": "23/07",
+    "titolo": "Arrivo a Gedda",
+    "attivita": [
+        {
+            "fascia": "Pomeriggio",
+            "titolo": "Volo per Gedda",
+            "descrizione": "RM-NA (WizzAir) 14:15–20:10 – N.Volo W46229",
+            "link": None,
+            "foto": None
+        },
+        {
+            "fascia": "Sera",
+            "titolo": "Check-in hotel",
+            "descrizione": """Rosemond Al Hamra - Al Uqsar Street Al Hamra District, Jeddah, Al Hamra, 21382 Gedda""",
+            "link": "https://www.booking.com/hotel/sa/address-al-hamra.it.html?aid=311984&label=address-al-hamra-r069upSh2TDMgbnVst8XXQS541222572163%3Apl%3Ata%3Ap1%3Ap2%3Aac%3Aap%3Aneg%3Afi%3Atikwd-2296550163331%3Alp1006094%3Ali%3Adec%3Adm%3Appccp%3DUmFuZG9tSVYkc2RlIyh9YTQUGSsRwx9_3qo3uPTHyoo&sid=3fce56a417f998954d5d00812c0ecde3&age=0&all_sr_blocks=395356801_398771686_2_2_0_411329&checkin=2025-07-27&checkout=2025-07-30&dest_id=-3096108&dest_type=city&dist=0&group_adults=2&group_children=1&hapos=1&highlighted_blocks=395356801_398771686_2_2_0_411329&hpos=1&matching_block_id=395356801_398771686_2_2_0_411329&no_rooms=1&req_adults=2&req_age=0&req_children=1&room1=A%2CA%2C0&sb_price_type=total&sr_order=popularity&sr_pri_blocks=395356801_398771686_2_2_0_411329_95362&srepoch=1745683896&srpvid=4a3471d508b50194&type=total&ucfs=1&",
+            "foto": None
+        },
+        {
+            "fascia": "Extra",
+            "titolo": "Posizione hotel",
+            "descrizione": "Visualizza su Google Maps",
+            "link": "https://maps.app.goo.gl/bB7WLqjEjQd3kiJ88?g_st=com.google.maps.preview.copy",  # puoi adattare le coordinate precise
+            "foto": None
+        }
+    ]
+},
+            {
+                "data": "2025-07-24",
+                "titolo": "Scoperta di Gedda",
+                "attivita": [
+                    {
+                        "fascia": "Mattina",
+                        "titolo": "Esplorazione libera",
+                        "descrizione": "Giornata a disposizione per visitare la città o rilassarsi.",
+                        "link": None,
+                        "foto": None
+                    }
+                ]
+            },
+            {
+                "data": "2025-07-25",
+                "titolo": "Giornata intera a Gedda",
+                "attivita": [
+                    {
+                        "fascia": "Intera giornata",
+                        "titolo": "Visite libere o tour opzionali",
+                        "descrizione": "Possibilità di escursioni, mare o centro storico.",
+                        "link": None,
+                        "foto": None
+                    }
+                ]
+            },
+            {
+                "data": "2025-07-26",
+                "titolo": "Partenza da Gedda",
+                "attivita": [
+                    {
+                        "fascia": "Mattina",
+                        "titolo": "Trasferimento e volo",
+                        "descrizione": "Volo in partenza per Jakarta. Eventuali variazioni per Singapore se cancellato.",
+                        "link": None,
+                        "foto": None
+                    }
+                ]
+            }
+        ]
+    }
+]
